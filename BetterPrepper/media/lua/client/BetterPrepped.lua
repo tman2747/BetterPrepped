72c2eb9ad82108c670515fe6250234ac991c74c6
Triston Land <tristonland@gmail.com>
2023-02-01 01:02:04 -0700

    Add Items to bags and README changes

--- BetterPrepper/media/lua/client/BetterPrepped.lua
+++ BetterPrepper/media/lua/client/BetterPrepped.lua
@@ -5,23 +5,47 @@ local function initBetterPreppedTrait()
 end
 
 local function initBetterPreppedItems(player, square)
+	bandageamount = 8
+	nailamount = 12
 	if player:HasTrait("BetterPrepped") then
 	        local BetterPrepped = player:getInventory():AddItem("Base.Bag_ALICEpack_Army");
 	        BetterPrepped:getItemContainer():AddItem("Base.WaterBottleFull");
-			BetterPrepped:getItemContainer():AddItem("Base.AlcoholBandage");
+			for i = 1,nailamount,1
+			do
+				BetterPrepped:getItemContainer():AddItem("Base.Nails");
+			end
+			BetterPrepped:getItemContainer():AddItem("Base.Screwdriver");
 			BetterPrepped:getItemContainer():AddItem("Base.TinOpener");
-			BetterPrepped:getItemContainer():AddItem("Base.Cigarettes");
 			BetterPrepped:getItemContainer():AddItem("Base.HuntingKnife");
-			BetterPrepped:getItemContainer():AddItem("Base.HuntingKnife");
+			BetterPrepped:getItemContainer():AddItem("Base.HandAxe");
+			BetterPrepped:getItemContainer():AddItem("Base.Hammer");
+			BetterPrepped:getItemContainer():AddItem("Base.BeefJerky");
+			BetterPrepped:getItemContainer():AddItem("Base.BeefJerky");
+
 
 local BetterPrepped = player:getInventory():AddItem("Base.Suitcase");
-	        BetterPrepped:getItemContainer():AddItem("Base.AlcoholBandage");
+			BetterPrepped:getItemContainer():AddItem("Base.AlcoholWipes");			
+			for i = 1,bandageamount,1
+			do
+				BetterPrepped:getItemContainer():AddItem("Base.AlcoholBandage");
+			end
 			BetterPrepped:getItemContainer():AddItem("Base.Scissors");
 			BetterPrepped:getItemContainer():AddItem("Base.Cigarettes");
 			BetterPrepped:getItemContainer():AddItem("Base.Lighter");
 			BetterPrepped:getItemContainer():AddItem("Base.Matches");
-			BetterPrepped:getItemContainer():AddItem("Base.BeefJerky");
-            BetterPrepped:getItemContainer():AddItem("Base.BeefJerky");
+            BetterPrepped:getItemContainer():AddItem("Base.Pills");
+			BetterPrepped:getItemContainer():AddItem("Base.PillsVitamins");
+			BetterPrepped:getItemContainer():AddItem("Base.BathTowel");
+			BetterPrepped:getItemContainer():AddItem("Base.Eraser");
+			BetterPrepped:getItemContainer():AddItem("Base.Pencil");
+			BetterPrepped:getItemContainer():AddItem("Base.Screwdriver");
+			BetterPrepped:getItemContainer():AddItem("Base.AlcoholWipes");
+			BetterPrepped:getItemContainer():AddItem("Base.PonchoGreen");
+
+
+
+
+
 
 	end
 end
